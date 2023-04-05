701b38052be6ad47ed276ee7b6f03b473492438b
Nick Jackson <nickdjackson@msn.com>
2023-04-05 12:44:10 -0400

    functions

--- test.php
+++ test.php
@@ -1,4 +1,21 @@
 <?php
 
-$catsName = 'Meowsalot';
-echo "The name of the cat is $catsName and that is the truth."; //Need double quotes for "template literal"
+// $catsName = 'Meowsalot';
+// echo "The name of the cat is $catsName and that is the truth."; Need double quotes for "template literal"
+
+
+function doubleMe($x) {
+    return $x * 2;
+}
+
+function tripleMe($x) {
+    return $x * 3;
+}
+
+$ourCoolNumber = tripleMe(doubleMe(50));
+
+if ($ourCoolNumber > 200) {
+    echo 'The number is large enough.';
+} else {
+    echo 'The number is not large enough';
+}
